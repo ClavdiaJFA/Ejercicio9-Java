27d5c5944c9ac563e5e8de110cb0a1229f4666b2
ClavdiaJFA <cjfa08102013@gmail.com>
2021-07-16 11:57:25 -0500

    Ejercicio9 Java

---
+++ Ejercicio9Java/src/tresnumeros.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class tresnumeros {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+/*Debe diseñar un programa que reciba 3 números por consola 
+y determine cual de los 3 números es el mayor.
+
+Plus: Imprimir los números ordenados de mayor a menor.
+Plus: Imprimir los números ordenados de menor a mayor.*/ç
+
+		Scanner num = new Scanner (System.in);
+		System.out.println("Introduce el primer número: ");
+		int numUno = num.nextInt();
+		System.out.println("Introduce el segundo número: ");
+		int numDos = num.nextInt();
+		System.out.println("Introduce el tercer número: ");
+		int numTres = num.nextInt();
+		
+		public static String
+		
+		String numMayor = ("");
+		
+		int = if num {
+				("numUno" > "numDos");
+				("numUno" > "numTres");
+				("numDos" > "numUno");
+				("numDos" > "numTres");
+				("numTres" > "numUno");
+				("numTres" > "numDos");
+		}
+		
+		return int = num > 
+				
+
+	}
+
+}
